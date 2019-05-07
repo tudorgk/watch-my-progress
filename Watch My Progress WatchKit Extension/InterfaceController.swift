fff41ecdd0731c06862a3e9f0d6bc3fcc4031ce3
Tudor Dragan <me@tudordev.com>
2019-05-07 10:03:11 +0300

    initial commit.

---
+++ Watch My Progress WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Watch My Progress WatchKit Extension
+//
+//  Created by Tudor Dragan on 07/05/2019.
+//  Copyright © 2019 Watch My Progress. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ Watch My Progress/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Watch My Progress
+//
+//  Created by Tudor Dragan on 07/05/2019.
+//  Copyright © 2019 Watch My Progress. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
