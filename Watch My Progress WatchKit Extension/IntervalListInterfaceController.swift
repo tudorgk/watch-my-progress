ae6a431f16f3dbe50e3e9d6e758ed7b8710439ac
Tudor Dragan <me@tudordev.com>
2019-05-07 15:47:50 +0300

    Created dummy list

--- Watch My Progress WatchKit Extension/IntervalListInterfaceController.swift
+++ Watch My Progress WatchKit Extension/IntervalListInterfaceController.swift
@@ -12,14 +12,21 @@ import WatchKit
 class IntervalListInterfaceController: WKInterfaceController {
         @IBOutlet weak var timerTable: WKInterfaceTable!
     
-    let headerType = "headerType"
-    let cellType = "cellType"
-    
+    let rowTypes = [HeaderController.type, RowController.type, RowController.type, HeaderController.type, RowController.type]
+    let rowDataSource = ["Mobile Meetup Hackathon", "10 sec", "10 min", "Boringc Project", "59 min 2 sec"]
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
-        timerTable.setRowTypes([headerType, cellType, cellType, headerType])
-    
+        timerTable.setRowTypes(rowTypes)
+        for (index, element) in rowTypes.enumerated() {
+            if element == HeaderController.type {
+                let header = timerTable.rowController(at: index) as! HeaderController
+                header.titleLabel.setText(rowDataSource[index])
+            } else {
+                let row = timerTable.rowController(at: index) as! RowController
+                row.titleLabel.setText(rowDataSource[index])
+            }
+        }
     }
     
     override func willActivate(){
